57fc0c87f2a9979ae688955d90a986a03c5530e3
Jordan Hall <jordan.hall@rapidweb.biz>
2016-03-01 11:03:21 +0000

    Various fixes

--- src/middleware/BasicAuthentication.php
+++ src/middleware/BasicAuthentication.php
@@ -5,31 +5,42 @@ namespace DivineOmega\LaravelRouteRestrictor\Middleware;
 use App;
 use Auth;
 use Closure;
+use Route;
+use Response;
 
 class BasicAuthentication
 {
   public function handle($request, Closure $next)
   {
-    if (!$this->validate($request->header('PHP_AUTH_USER'), $request->header('PHP_AUTH_PW'))) {
-        App::abort(403);
+    $username = $request->getUser();
+    $password = $request->getPassword();
+
+    if (!$this->validate($request,$username, $password)) {
+      header('WWW-Authenticate: Basic');
+      App::abort(401, 'Unauthorized. Please check your username and password.');
     }
     return $next($request);
   }
 
-  private function validate($user, $password)
+  private function validate($request, $user, $password)
   {
     // Get current route name
-    $routeName = \Request::route()->getName();
+    // Note: we do not have access to the current route in middleware, because
+    // it has not been fully dispatched, therefore we must use the backwards
+    // method of finding the route which matches the current request.
+    $routeName = Route::getRoutes()->match($request)->getName();
 
     // If we have a named route
     if ($routeName) {
 
       // Check if route username and password are set
-      if ($routeUsername = env('routerestrictor.route.'.$routeName.'.username') && $routePassword = env('routerestrictor.route.'.$routeName.'.password')) {
+      if ($routeUsername = env('ROUTE_RESTRICTOR_ROUTE_'.strtoupper($routeName).'_USERNAME') && $routePassword = env('ROUTE_RESTRICTOR_ROUTE_'.strtoupper($routeName).'_PASSWORD')) {
 
         // Check against route password
-        if (trim($user) === $routeUsername && trim($password) === $routePassword) {
+        if (trim($user) == $routeUsername && trim($password) == $routePassword) {
           return true;
+        } else {
+          return false;
         }
       }
 
@@ -37,16 +48,18 @@ class BasicAuthentication
 
 
     // Check if global username and password are set
-    if ($globalUsername = env('routerestrictor.global.username') && $globalPassword = env('routerestrictor.global.password')) {
+    if ($globalUsername = env('ROUTE_RESTRICTOR_GLOBAL_USERNAME') && $globalPassword = env('ROUTE_RESTRICTOR_GLOBAL_PASSWORD')) {
 
       // Check against global password
-      if (trim($user) === $globalUsername && trim($password) === $globalPassword) {
+      if (trim($user) == $globalUsername && trim($password) == $globalPassword) {
         return true;
+      } else {
+        return false;
       }
 
     }
 
-    return false;
+    return true;
   }
 
 }
